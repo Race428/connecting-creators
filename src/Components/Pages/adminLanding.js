020e3ef3d251e65f11ed42ae6e71415b1d6f964f
Race428 <raceacheson@gmail.com>
2019-06-17 17:13:56 -0600

    added login

--- src/Components/Pages/adminLanding.js
+++ src/Components/Pages/adminLanding.js
@@ -17,24 +17,18 @@ export default class LandingPage extends Component {
         // Dashboard component will need to grab session first
 
         const session = await axios.get('/checkSession') 
-        try {
+        
             console.log(session)
-            session.data.admin ? 
-            await this.props.updateUser(session.data.admin) :
-            await this.props.updateUser(session.data.user)
-        } catch {
-            this.props.history.push('/')
-        }
 
-        if(this.props.companyId) {
-           this.setState({
-               admin: true
-           }) 
-        } else {
-            this.setState ({
-                user: true
-            })
-        } 
+            if( session.data.admin === true){
+                console.log('ok')
+            }
+            else{
+                this.props.history.push('/')
+            }
+          
+
+       
         
         // at this point, we would grab the job postings, applicants for
         // those job postings, and various bits of company info
