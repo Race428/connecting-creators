d4e4fa1a2b848e636f63f13d3d5d139874b458d4
Race428 <raceacheson@gmail.com>
2019-07-16 20:03:49 -0600

    css

--- src/Components/Pages/Store/step1.js
+++ src/Components/Pages/Store/step1.js
@@ -20,7 +20,7 @@ class Step1 extends Component {
             package1: 'package1',
             package2: 'package2',
             package3: 'package3',
-      
+
             // selected:''
         }
 
@@ -30,54 +30,56 @@ class Step1 extends Component {
     componentDidMount() {
         if (this.props.packageSelection.length > 0) {
             var name = `${this.props.packageSelection}`
-
             this.setState({
                 [name]: style.selected,
                 selected: `${name}`
             })
 
+
         }
+console.log(this.state.selected)
+
     }
 
     componentDidUpdate = () => {
         this.props.updatePackageSelection(this.state.selected)
-       
-        if(this.state.package1 === 'selected'){
-            console.log('hi')
-            var y = document.getElementsByName('option1-image');
-            y[0].setAttribute('id', 'option1-image-active');
-    }
-    else { 
-        var y = document.getElementsByName('option1-image');
-        y[0].setAttribute('id', 'option1-image');
-    }
+
+        //         if(this.state.package1 === 'selected'){
+        //             console.log('hi')
+        //             var y = document.getElementsByName('option1-image');
+        //             y[0].setAttribute('id', 'option1-image-active');
+        //     }
+        //     else { 
+        //         var y = document.getElementsByName('option1-image');
+        //         y[0].setAttribute('id', 'option1-image');
+        //     }
 
 
 
-    if(this.state.package2 === 'selected'){
-  
-        var y = document.getElementsByName('option2-image');
-        y[0].setAttribute('id', 'option2-image-active');
-}
-else { 
-    var y = document.getElementsByName('option2-image');
-    y[0].setAttribute('id', 'option2-image');
-}
-     
+        //     if(this.state.package2 === 'selected'){
+
+        //         var y = document.getElementsByName('option2-image');
+        //         y[0].setAttribute('id', 'option2-image-active');
+        // }
+        // else { 
+        //     var y = document.getElementsByName('option2-image');
+        //     y[0].setAttribute('id', 'option2-image');
+        // }
 
 
-if(this.state.package3 === 'selected'){
-  
-    var y = document.getElementsByName('option3-image');
-    y[0].setAttribute('id', 'option3-image-active');
-}
-else { 
-var y = document.getElementsByName('option3-image');
-y[0].setAttribute('id', 'option3-image');
-}
+
+        // if(this.state.package3 === 'selected'){
+
+        //     var y = document.getElementsByName('option3-image');
+        //     y[0].setAttribute('id', 'option3-image-active');
+        // }
+        // else { 
+        // var y = document.getElementsByName('option3-image');
+        // y[0].setAttribute('id', 'option3-image');
+        // }
     }
     package1Select = async () => {
-       await this.setState({
+        await this.setState({
             package1: 'selected',
             package2: 'package2',
             package3: 'package3',
@@ -87,7 +89,7 @@ y[0].setAttribute('id', 'option3-image');
 
 
 
-     package2Select = async () => {
+    package2Select = async () => {
         await this.setState({
             package1: 'package1',
             package2: 'selected',
@@ -96,8 +98,8 @@ y[0].setAttribute('id', 'option3-image');
 
         });
 
-      
-      
+
+
 
     }
 
@@ -110,7 +112,7 @@ y[0].setAttribute('id', 'option3-image');
             selected: 'package3'
 
         });
-       
+
 
 
 
@@ -134,6 +136,10 @@ y[0].setAttribute('id', 'option3-image');
 
     render() {
 
+        // if( this.state.selected === 'package1') { 
+        //     const image = 'series series series series series series series series series series series series series series series series series series series series series series series series series series'
+        // }
+
 
 
         return (
@@ -146,13 +152,28 @@ y[0].setAttribute('id', 'option3-image');
 
                             <div className={style.currentDisplay}>
 
+                                {
+                                    this.state.selected === 'package1' ?
+                                        <img src='https://images.unsplash.com/photo-1526660690293-bcd32dc3b123?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60' alt='fs' id='option1-image' name='option1-image' /> :
+                                        <></>
+                                }
 
-                                <img src='https://images.unsplash.com/photo-1526660690293-bcd32dc3b123?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60' alt='fs' id='option1-image' name='option1-image' />
-                                <img src='https://images.unsplash.com/photo-1534361960057-19889db9621e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60' alt='fs' id='option2-image' name='option2-image' />
+                                {
+                                    this.state.selected === 'package2' ?
+                                        <img src='https://images.unsplash.com/photo-1534361960057-19889db9621e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60' alt='fs' id='option2-image' name='option2-image' /> :
+                                        <> </>
 
-                              
-                                <img src=' https://images.unsplash.com/photo-1507146426996-ef05306b995a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60' alt='fs' id='option3-image' name='option3-image' />
-                               
+                                }
+
+
+
+                                {
+                                    this.state.selected === 'package3' ?
+                                        <img src=' https://images.unsplash.com/photo-1507146426996-ef05306b995a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60' alt='fs' id='option3-image' name='option3-image' /> :
+                                        <> </>
+                                }
+
+
                             </div>
 
 
@@ -186,6 +207,27 @@ y[0].setAttribute('id', 'option3-image');
                             <div className={style.optionDescription}>
 
 
+                            {
+                                    this.state.selected === 'package1' ?
+                                       <p className={style.description}>series series series series series series series series series series series series series series series series series series series series series series series series series series </p>:
+                                        <></>
+                                }
+
+                                {
+                                    this.state.selected === 'package2' ?
+                                    <p className={style.description}> Podcast Podcast Podcast PodcastPodcast PodcastPodcast PodcastPodcast PodcastPodcast PodcastPodcast PodcastPodcast PodcastPodcast PodcastPodcast PodcastPodcast PodcastPodcast PodcastPodcast PodcastPodcast PodcastPodcast PodcastPodcast PodcastPodcast PodcastPodcast PodcastPodcast Podcast </p>  
+                                    :
+                                        <> </>
+
+                                }
+
+
+
+                                {
+                                    this.state.selected === 'package3' ?
+                                       <p className={style.description}>Youtube Youtube Youtube Youtube Youtube Youtube Youtube Youtube Youtube Youtube Youtube Youtube Youtube Youtube Youtube Youtube Youtube Youtube Youtube Youtube Youtube Youtube Youtube Youtube Youtube Youtube Youtube Youtube Youtube </p>:
+                                        <> </>
+                                }
 
                             </div>
 
