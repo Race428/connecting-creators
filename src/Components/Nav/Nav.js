7c845e3c7397c6954bd4b898ce7a343b52b5a516
Race428 <raceacheson@gmail.com>
2019-07-22 19:52:40 -0600

    working on temple mill

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,46 +1,51 @@
 import React, { Component } from 'react'
 import navStyles from '../../CSS/nav.module.css'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 
 // import Logo from '../Images/logo.PNG'
 
 class Nav extends Component {
-   
-   componentDidMount(){
+
+    componentDidMount() {
 
 
-       
-   }
+
+    }
     render() {
         return (
             <>
 
-           
-            <nav>
-               <Link to='/'>
-                <img className={navStyles.logo} src='https://s3-us-west-1.amazonaws.com/achesonco/BlackConnecting.png ' alt='logo' /> 
-                </Link>
-               
-                <Link to='/podcasts'>
-                <div className={navStyles.link}>
-                    Interviews
+
+                <nav>
+
+                    <div className={navStyles.topRow}>
+                        <Link to='/podcasts'>
+                            <div className={navStyles.link}>
+                                Interviews
+                             </div>
+                        </Link>
+
+                        <Link to='/shop/select-package'>
+                            <div className={navStyles.link}>
+                                Shop
                 </div>
-                </Link>
-                
-                <Link to='/shop/select-package'>
-                <div className={navStyles.link}>
-                    Shop
-                </div>
-                </Link>
-               
-                {/* <Link to=''>
+                        </Link>
+                    </div>
+                    <div className={navStyles.templeMill}>
+                        <Link className={navStyles.link} to='/'>
+                            <img className={navStyles.logo} src='https://s3-us-west-1.amazonaws.com/achesonco/temple.png ' alt='logo' />
+                        </Link>
+                    </div>
+
+
+                    {/* <Link to=''>
                 <div className={navStyles.link}>
                     About 
                 </div>
                 </Link> */}
 
-            </nav>
+                </nav>
             </>
 
         )
--- src/Components/Pages/landingPage.js
+++ src/Components/Pages/landingPage.js
@@ -3,10 +3,10 @@ import React, { Component } from 'react'
 import axios from 'axios';
 import landingStyles from '../../CSS/landingPage.module.css'
 import hero from '../Images/pic.JPG'
-import {updatePodcasts} from '../../Redux/Reducer'
+import { updatePodcasts } from '../../Redux/Reducer'
 
-import {connect} from 'react-redux'
-import {withRouter} from 'react-router-dom'
+import { connect } from 'react-redux'
+import { withRouter } from 'react-router-dom'
 
 
 class LandingPage extends Component {
@@ -14,29 +14,35 @@ class LandingPage extends Component {
 
 
 
- componentDidMount(){
-    axios.get('/api/podcasts/getall').then((res => {
-      
-       this.props.updatePodcasts(res.data)
-    
-    }))
- }
+    componentDidMount() {
+        axios.get('/api/podcasts/getall').then((res => {
+
+            this.props.updatePodcasts(res.data)
+
+        }))
+    }
 
 
 
     render() {
-      
+
 
         return (
             <>
-            <div className={landingStyles.pageView}>
-           <h1>Connecting Creators is a community where creatives can network, learn, and grow.</h1>
-           
-           {/* <div className={landingStyles.imageBox}>
-            <img className={landingStyles.mike} src='https://www.arohatours.co.nz/media/1645/banner_queenstown-g.jpg?mode=crop&height=700&width=1920&quality=80' alt='mike' /> 
-            <caption> <a  href='https://www.instagram.com/trumylin/'target='_blank'>@trumylin</a> and <a  href='https://www.instagram.com/kensingtoncharles/'target='_blank'>@kensingtoncharles</a>  at the base of Mount Bukhansan, South Korea</caption>
-            </div> */}
-            </div>
+                <div className={landingStyles.pageView}>
+                    <h1>Connecting Creators is a community where creatives can network, learn, and grow.</h1>
+
+<div>
+                    {/* <iframe  className={landingStyles.video}  src="https://www.youtube.com/embed/5ebz0NIGgOc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen /> 
+                 */}
+
+                    </div>
+                   
+
+                 
+
+
+                </div>
             </>
         )
     }
@@ -45,9 +51,9 @@ class LandingPage extends Component {
 
 
 
-const mapStateToProps =state=> {
+const mapStateToProps = state => {
     return {
-        ...state 
+        ...state
     }
 }
 
